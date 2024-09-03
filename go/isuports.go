d3fd3af96f5384ced1ebd4d47377fd6eee594dbe
Arata <arata04962@gmail.com>
2024-09-03 15:55:01 +0900

    optimize inset player

--- go/isuports.go
+++ go/isuports.go
@@ -794,6 +794,8 @@ func playersAddHandler(c echo.Context) error {
 	}
 	displayNames := params["display_name[]"]
 
+	now := time.Now().Unix()
+	playerRows := make([]PlayerRow, 0, len(displayNames))
 	pds := make([]PlayerDetail, 0, len(displayNames))
 	for _, displayName := range displayNames {
 		id, err := dispenseID(ctx)
@@ -801,26 +803,32 @@ func playersAddHandler(c echo.Context) error {
 			return fmt.Errorf("error dispenseID: %w", err)
 		}
 
-		now := time.Now().Unix()
-		if _, err := tenantDB.ExecContext(
-			ctx,
-			"INSERT INTO player (id, tenant_id, display_name, is_disqualified, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)",
-			id, v.tenantID, displayName, false, now, now,
-		); err != nil {
-			return fmt.Errorf(
-				"error Insert player at tenantDB: id=%s, displayName=%s, isDisqualified=%t, createdAt=%d, updatedAt=%d, %w",
-				id, displayName, false, now, now, err,
-			)
+		playerRow := PlayerRow{
+			ID:             id,
+			TenantID:       v.tenantID,
+			DisplayName:    displayName,
+			IsDisqualified: false,
+			CreatedAt:      now,
+			UpdatedAt:      now,
 		}
-		p, err := retrievePlayer(ctx, tenantDB, id)
-		if err != nil {
-			return fmt.Errorf("error retrievePlayer: %w", err)
-		}
+		playerRows = append(playerRows, playerRow)
+
 		pds = append(pds, PlayerDetail{
-			ID:             p.ID,
-			DisplayName:    p.DisplayName,
-			IsDisqualified: p.IsDisqualified,
+			ID:             playerRow.ID,
+			DisplayName:    playerRow.DisplayName,
+			IsDisqualified: playerRow.IsDisqualified,
 		})
+	}
+
+	if _, err := tenantDB.ExecContext(
+		ctx,
+		"INSERT INTO player (id, tenant_id, display_name, is_disqualified, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)",
+		playerRows,
+	); err != nil {
+		return fmt.Errorf(
+			"error Insert player at tenantDB: %w",
+			err,
+		)
 	}
 
 	res := PlayersAddHandlerResult{
