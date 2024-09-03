6bcb5e4936df5e76a4587f6d05b2ef1b117043e4
Arata <arata04962@gmail.com>
2024-09-03 14:31:54 +0900

    use pprotein

--- go/isuports.go
+++ go/isuports.go
@@ -21,6 +21,7 @@ import (
 	"github.com/go-sql-driver/mysql"
 	"github.com/gofrs/flock"
 	"github.com/jmoiron/sqlx"
+	"github.com/kaz/pprotein/integration/echov4"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
@@ -136,6 +137,8 @@ func Run() {
 	e := echo.New()
 	e.Debug = true
 	e.Logger.SetLevel(log.DEBUG)
+
+	echov4.EnableDebugHandler(e)
 
 	var (
 		sqlLogger io.Closer
