79c10da625d12eea68a9edf949ebd061615f1633
Arata <arata04962@gmail.com>
2024-09-03 18:06:29 +0900

    use transaction instead of flock

--- go/isuports.go
+++ go/isuports.go
@@ -1041,12 +1041,6 @@ func competitionScoreHandler(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, "invalid CSV headers")
 	}
 
-	// / DELETEしたタイミングで参照が来ると空っぽのランキングになるのでロックする
-	fl, err := flockByTenantID(v.tenantID)
-	if err != nil {
-		return fmt.Errorf("error flockByTenantID: %w", err)
-	}
-	defer fl.Close()
 	var rowNum int64
 	playerScoreRows := []PlayerScoreRow{}
 	for {
@@ -1096,7 +1090,12 @@ func competitionScoreHandler(c echo.Context) error {
 		})
 	}
 
-	if _, err := tenantDB.ExecContext(
+	tx, err := tenantDB.Beginx()
+	if err != nil {
+		return fmt.Errorf("error Beginx: %w", err)
+	}
+
+	if _, err := tx.ExecContext(
 		ctx,
 		"DELETE FROM player_score WHERE tenant_id = ? AND competition_id = ?",
 		v.tenantID,
@@ -1104,7 +1103,7 @@ func competitionScoreHandler(c echo.Context) error {
 	); err != nil {
 		return fmt.Errorf("error Delete player_score: tenantID=%d, competitionID=%s, %w", v.tenantID, competitionID, err)
 	}
-	if _, err := tenantDB.NamedExecContext(
+	if _, err := tx.NamedExecContext(
 		ctx,
 		"INSERT INTO player_score (id, tenant_id, player_id, competition_id, score, row_num, created_at, updated_at) VALUES (:id, :tenant_id, :player_id, :competition_id, :score, :row_num, :created_at, :updated_at)",
 		playerScoreRows,
@@ -1112,6 +1111,10 @@ func competitionScoreHandler(c echo.Context) error {
 		return fmt.Errorf(
 			"error Insert player_score: %w", err,
 		)
+	}
+
+	if err = tx.Commit(); err != nil {
+		return fmt.Errorf("error Commit: %w", err)
 	}
 
 	return c.JSON(http.StatusOK, SuccessResult{
